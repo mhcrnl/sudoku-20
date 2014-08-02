c757bfac8ac415f8ca4a4d1cf0d23c196680c7cb
Blahmaster <Blahmaster@users.noreply.github.com>
2014-08-02 14:52:19 +0200

    Changed some stuff. And added a new random fill function.

--- Source/Sudoku_array.cc
+++ Source/Sudoku_array.cc
@@ -17,18 +17,29 @@ char read_char()
 //Returns the first single integer decimal in the input
 int read_int()
 {
-	int n;
 	char car = ' ';
 	while(car < '0' || car > '9')
 	{
 		cin.get(car);
-		if ((car >= '0') && (car <='9'))
-		{
-			n = car - '0';
-			return n;
-		}
-	}
+	}//while
+	return car - '0';
 }//read_int
+
+// Draws a pretty line
+void fancy_line()
+{
+	int j;
+	for(j = 0; j <= 9; j++)
+	{
+		if(j%3 == 0)
+		{
+			cout << "+";
+		}//if
+		if(j != 9)
+			cout << "-";
+	}//for
+	cout << "\n";
+}//fancy_line
 
 // Class sudoku
 class sudoku
@@ -37,6 +48,8 @@ class sudoku
 		int grid[9][9];
 		bool check_row(int);
 		bool check_col(int);
+		bool int_in_row(int,int);
+		bool int_in_col(int,int);
 	public:
 		sudoku();
 		void draw();
@@ -58,9 +71,10 @@ sudoku::sudoku()
 void sudoku::draw()
 {
 	int i,j;
-	cout << "\n";
+	fancy_line();
 	for(i = 0; i < 9; i++)
 	{
+		cout << "|";
 		for(j = 0; j < 9; j++)
 		{
 			if(grid[i][j] == 0)
@@ -72,7 +86,7 @@ void sudoku::draw()
 				cout << grid[i][j];
 			}//else
 			if(j%3 == 2)
-				cout << " ";
+				cout << "|";
 		}//for
 		if(check_row(i))
 		{
@@ -83,9 +97,11 @@ void sudoku::draw()
 			cout << " X\n";
 		}//else
 		if(i%3 == 2)
-			cout << "\n";
-	}
-
+		{
+			fancy_line();
+		}//if
+	}//for
+	cout << " ";
 	for(j = 0; j < 9; j++)
 	{
 		if(check_col(j))
@@ -152,32 +168,78 @@ void sudoku::set_cell(int x, int y, int n)
 	grid[y][x] = n;
 }//sudoku::set_num
 
-// Fills the grid with random integers from 1-9
-void sudoku::fill_rand(int n)
+// Returns true if the integer is present in the given row
+bool sudoku::int_in_row(int n, int row)
+{
+	int j;
+	for(j = 0; j < 9; j++)
+	{
+		if(grid[row][j] == n)
+		{
+			return true;
+		}//if
+	}//for
+	return false;
+}//sudoku::int_in_row
+
+// Returns true if the integer is present in the given col
+bool sudoku::int_in_col(int n, int col)
 {
 	int i;
-	for(i = 0; i < n; i++)
+	for(i = 0; i < 9; i++)
 	{
-		grid[rand()%9][rand()%9] = rand()%9 + 1;
+		if(grid[i][col] == n)
+		{
+			return true;
+		}//if
 	}//for
+	return false;
+}//sudoku::int_in_row
+
+// Fills the grid with random integers from 1-9
+void sudoku::fill_rand(int m)
+{
+	int k = 0;
+	int x, y, n;
+	while(k < m)
+	{
+		x = rand()%9;
+		y = rand()%9;
+		if(grid[y][x] == 0)
+		{
+			n = rand()%9 + 1;
+			if(!int_in_col(n, x) && !int_in_row(n, y))
+			{
+				grid[y][x] = n;
+				k++;
+			}//if
+		}//if
+	}//while
 }//fill_rand
 
 // Menu where the user can preform actions
 void menu(sudoku &sud)
 {
 	int n;
+	int x,y;
 	char car = ' ';
 	cout << "Welcome to this humble commandline implementation of sudoku" << endl;
 
 	while(car != 'E')
 	{
-		cout << "\n[S]et cell, [D]raw, [R]andom fill\n";
+		cout << "\n[S]et cell, [D]raw, [R]andom fill, [E]xit\n";
 		car = read_char();
 		switch(car)
 		{
 			case 'S':
-				cout << "Give the location of the cell.\n";
-				sud.set_cell(read_int(),read_int(),read_int());
+				cout << "Give the location of the cell.\nx= ";
+				x = read_int();
+				cout << "y= ";
+				y = read_int();
+				cout << "n= ";
+				n = read_int();
+				cout << "\n";
+				sud.set_cell(x-1,y-1,n);
 				break;
 			case 'D':
 				cout << "Drawing the grid.\n";
@@ -188,7 +250,7 @@ void menu(sudoku &sud)
 				cin >> n;
 				sud.fill_rand(n);
 				break;
-		}
+		}//switch
 		sud.draw();
 	}//while
 }//menu
